e8957cb592ce631412e5f19205905d64327ca935
Bridgette <53711858+bshub6@users.noreply.github.com>
2019-11-02 16:50:22 -0400

    adding working

--- app.py
+++ app.py
@@ -1,25 +1,31 @@
+#Dependencies
+from flask import Flask, jsonify
 import numpy as np
-
+import pandas as pd
+import dateline as dt
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask, jsonify
-
 
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///titanic.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
 
-# Save reference to the table
-Passenger = Base.classes.passenger
+#variables for bases
+measurement = Base.classes.measurement
+station = Bases.classes.station
+
+session = Session(engine)
+
 
 #################################################
 # Flask Setup
@@ -30,55 +36,64 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def home():
+    print ("Server received request for 'Home' page")
+    return "Welcome to the Surf's Up Weather API"
+
 
 @app.route("/")
 def welcome():
     """List all available api routes."""
     return (
+        f"Welcome to the Hawaii recipitation and Temperature Information API.<br/>"
         f"Available Routes:<br/>"
-        f"/api/v1.0/names<br/>"
-        f"/api/v1.0/passengers"
+        f"Here are the available API Calls:<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start date<br/>"
+        
     )
-
-
-@app.route("/api/v1.0/names")
-def names():
-    # Create our session (link) from Python to the DB
+#precipation API call
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    
+    #Create a session to link Python to the DB
     session = Session(engine)
-
-    """Return a list of all passenger names"""
-    # Query all passengers
-    results = session.query(Passenger.name).all()
-
+    last12 = dt.date(2015, 7, 17) - dt.timedelta(days=365)
+    precip_query = session.query(measurement.date, measurement.prcp).filter(measurement.date>=last12).all()
+    precip =  {dt.date(prcp for date, prcp in precip_query)}
+    
     session.close()
+    
+    return jsonify(precip)
+           
+#station API call
 
-    # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
-
-    return jsonify(all_names)
-
-
-@app.route("/api/v1.0/passengers")
-def passengers():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # Query all passengers
-    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
-
+@app.route("/api/v1.0/stations")
+def stations():
+    station_query = session.query(station.station, station.name).all()
+    station - list(np.ravel(station_query))
+    
     session.close()
+    
+    return jsonify(station)
 
-    # Create a dictionary from the row data and append to a list of all_passengers
-    all_passengers = []
-    for name, age, sex in results:
-        passenger_dict = {}
-        passenger_dict["name"] = name
-        passenger_dict["age"] = age
-        passenger_dict["sex"] = sex
-        all_passengers.append(passenger_dict)
+#temp API call
+app.route("/api/v1.0/tobs")
+def Temperature():
+    last12 = dt.date(2017, 7, 1) - dt.timedelta(days365)
+    temp = session.query(measurement.date, measurement.tobs).filter(measurement.date >= last12).all()
+    tobs = list(np.ravel(temp))
+    
+    session.close()
+    
+    return jsonify(tobs)
 
-    return jsonify(all_passengers)
+
+
+
 
 
 if __name__ == '__main__':
